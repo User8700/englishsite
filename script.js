838cca0957092a20ec6a3668e69d86d978818e40
User8700 <generalmakarik@mail.ru>
2023-12-01 20:11:31 +0300

    Added main.html

---
+++ script.js
@@ -0,0 +1,12 @@
+let prevBtn = document.querySelector('.page-button1')
+let nextBtn = document.querySelector('.page-button2')
+
+
+
+nextBtn.addEventListener('click', function() {
+
+})
+
+prevBtn.addEventListener('click', function() {
+    
+})
